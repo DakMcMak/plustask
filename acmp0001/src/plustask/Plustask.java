bf664a585ae41b0d952f4f7256aafbf41077b69d
Анара <@Anara>
2017-02-14 19:49:27 +0600

    Version 0.0.1

---
+++ acmp0001/src/plustask/Plustask.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plustask;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Анара
+ */
+public class Plustask {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.print(a+b);
+    }
+    
+}
